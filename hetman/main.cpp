95aafa8a7c2027c874903bcf777d4a5d36c787b7
ISaidMe0w <81910661+isaidme0w@users.noreply.github.com>
2021-11-28 23:00:17 +0100

    Arrangement
    removed some unneeded lines

--- hetman/main.cpp
+++ hetman/main.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 int main() {
+	
 	int field[8][8]={0},x,y,a;
+	
 	do {
 		cout << "Number of row: ";
 		cin >> x;
@@ -11,39 +13,45 @@ int main() {
 		cin >> y;
 	}
 	while(x<0 || x>8 || y<0 || y>8);
+	
 	a = y;
 	field[x][y] = 2;
-	for(int i=x;i>0;i--) {
-		field[i-1][y] = 1;
-	}
-	for(int i=x;i<7;i++) {
-		field[i+1][y] = 1;
-	}
-	for(int i=y;i>0;i--) {
-		field[x][i-1] = 1;
-	}
-	for(int i=y;i<7;i++) {
-		field[x][i+1] = 1;
-	}
+	
+	for(int i=x;i>0;i--)
+	    field[i-1][y] = 1;
+	
+	for(int i=x;i<7;i++)
+	    field[i+1][y] = 1;
+	
+	for(int i=y;i>0;i--)
+	    field[x][i-1] = 1;
+	
+	for(int i=y;i<7;i++)
+	    field[x][i+1] = 1;
+	
 	for(int i=x+1;i<8 && a>0;i++) {
 		a--;
 		field[i][a] = 1;
 	}
+	
 	a = y;
 	for(int i=x+1;i<8 && a<7;i++) {
 		a++;
 		field[i][a] = 1;
 	}
+	
 	a = y;
 	for(int i=x-1;i<=8 && a<7;i--) {
 		a++;
 		field[i][a] = 1;
 	}
+	
 	a = y;
 	for(int i=x-1;i<=8 && a>0;i--) {
 		a--;
 		field[i][a] = 1;
 	}
+	
 	for(int i=0;i<8;i++) {
 	    for(int j=0;j<8;j++) cout << field[i][j] << " ";
 		cout << endl;

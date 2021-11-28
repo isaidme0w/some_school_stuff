cd8d82f7e1d4d405718e11ae8194fa9567a8aa5a
ISaidMe0w <81910661+isaidme0w@users.noreply.github.com>
2021-11-28 22:56:16 +0100

    Finished hetman
    Hetman now works correctly

--- hetman/main.cpp
+++ hetman/main.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 
 int main() {
-	int field[8][8]={0},x,y;
+	int field[8][8]={0},x,y,a;
 	do {
-		cout << "Podaj wiersz: ";
+		cout << "Number of row: ";
 		cin >> x;
-		cout << "Podaj kolumne: ";
+		cout << "Number of column: ";
 		cin >> y;
 	}
 	while(x<0 || x>8 || y<0 || y>8);
+	a = y;
 	field[x][y] = 2;
 	for(int i=x;i>0;i--) {
 		field[i-1][y] = 1;
@@ -23,6 +24,25 @@ int main() {
 	}
 	for(int i=y;i<7;i++) {
 		field[x][i+1] = 1;
+	}
+	for(int i=x+1;i<8 && a>0;i++) {
+		a--;
+		field[i][a] = 1;
+	}
+	a = y;
+	for(int i=x+1;i<8 && a<7;i++) {
+		a++;
+		field[i][a] = 1;
+	}
+	a = y;
+	for(int i=x-1;i<=8 && a<7;i--) {
+		a++;
+		field[i][a] = 1;
+	}
+	a = y;
+	for(int i=x-1;i<=8 && a>0;i--) {
+		a--;
+		field[i][a] = 1;
 	}
 	for(int i=0;i<8;i++) {
 	    for(int j=0;j<8;j++) cout << field[i][j] << " ";
